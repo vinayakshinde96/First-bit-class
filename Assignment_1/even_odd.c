510c7cbdd2dfc84ac21bdb1cfc9e9bd32ee0a390
Vinayak Shinde <shindevinayak2727@gmail.com>
2026-06-10 16:42:00 +0530

    Assignment_1
    codes

---
+++ Assignment_1/even_odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num = 6;
+    if(num%2==0)
+    {
+        printf("Number is even");
+    }
+    else{
+        printf("Number is odd");
+    }
+}
---
+++ Assignment_1/leap_year.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int year = 2023;
+    if(year%4 == 0)
+    {
+        printf("Year is leap");
+    }
+    else
+    {
+        printf("Year is not leap");
+    }
+}
